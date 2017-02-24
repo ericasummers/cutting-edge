18139e4c3fad463b37bec9b67cf2f6977b955ed7
Erica Wright <ericaw21@gmail.com>
2017-02-24 08:12:29 -0800

    create getters and setters for Stylist and Client classes

---
+++ src/Client.php
@@ -0,0 +1,51 @@
+<?php
+    class Client
+    {
+        private $name;
+        private $phone_number;
+        private $stylist_id;
+        private $id;
+
+        function __construct($name, $phone_number, $stylist_id, $id = null)
+        {
+            $this->name = $name;
+            $this->phone_number = $phone_number;
+            $this->stylist_id = $stylist_id;
+            $this->id = $id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+
+        function getPhoneNumber()
+        {
+            return $this->phone_number;
+        }
+
+        function setPhoneNumber($new_phone_number)
+        {
+            $this->phone_number = (int) $new_phone_number;
+        }
+
+        function getStylistId()
+        {
+            return $this->stylist_id;
+        }
+
+        function setStylistId($new_stylist_id)
+        {
+            $this->stylist_id = (int) $new_stylist_id;
+        }
+        
+    }
+
+
+
+?>
---
+++ src/Stylist.php
@@ -0,0 +1,43 @@
+<?php
+    class Stylist
+    {
+        private $name;
+        private $specialty;
+        private $id;
+
+        function __construct($new_name, $new_specialty, $id = null)
+        {
+            $this->name = $new_name;
+            $this->specialty = $specialty;
+            $this->id = $id;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setName($new_name)
+        {
+            $this->id = (string) $new_name;
+        }
+
+        function getSpecialty()
+        {
+            return $this->specialty;
+        }
+
+        function setSpecialty($new_specialty)
+        {
+            $this->specialty = (string) $new_specialty;
+        }
+    }
+
+
+
+?>
