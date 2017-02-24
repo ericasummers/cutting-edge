71eae86a3e3142359c2bcb5ff4a909cd45b48552
Erica Wright <ericaw21@gmail.com>
2017-02-24 10:18:22 -0800

    create index.php and app.php pages

---
+++ app/app.php
@@ -0,0 +1,27 @@
+<?php
+    date_default_timezone_set('America\Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Stylist.php";
+    require_once __DIR__."/../src/Client.php";
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost:3306;dbname=hair_salon';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app['debug'] = true;
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('home.html.twig', array('stylists' => Stylist::getAll());
+    });
+
+    return $app;
+    
+?>
